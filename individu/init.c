d90db6b520f4a5862b2ae94dad6449ca040199a7
Galyfray <galyfray@gmail.com>
2019-10-09 09:54:31 +0200

    ajout d'un début de header et des algo pour les 2 premier point de la q°1

---
+++ individu/init.c
@@ -0,0 +1,15 @@
+/*
+ -Nom: initialisation de la structure individu
+*/
+
+#include "individu.h"
+#include <time.h>
+
+Individu indv_init(){
+    
+    int i;
+    Individu indv = {NULL};
+    srand(time(0));
+    
+    
+}
