137d52a675bbcb9ee72304cb9a263d23ea8ef57c
52Alexis <30575465+52Alexis@users.noreply.github.com>
2019-10-30 18:32:34 +0100

    header pop

--- pop/pop.c
+++ pop/pop.c
@@ -1,5 +1,6 @@
 
-#include "LO21\bit\bits.h"
+#include "..\bit\bits.h"
+#include "..\individu\individu.h"
 #include "pop.h"
 #include <time.h>
 #include <stdlib.h>
--- pop/pop.h
+++ pop/pop.h
@@ -1,9 +1,11 @@
 
-#include "LO21\bit\bits.h"
+#include "..\bit\bits.h"
+#include "..\individu\individu.h"
 
 /* Structure représentant une liste chainée d'individu */
 typedef struct BitsList{
-    int qual;
+    float qual;
+	Individu Indiv
     struct BitsList* next;
 	struct BitsList* prev;
 } Population;
